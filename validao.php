837ced060fc60dd3db4fc4e33aba31ea2dbc4f92
Salas <43351197+XSalas@users.noreply.github.com>
2023-05-25 11:36:46 +0200

    Add files via upload

--- validao.php
+++ validao.php
@@ -9,7 +9,15 @@ if (
   header("Location: login.php");
   exit();
 }
+
+// Check if the button for "Profes" or "Alumnos" is clicked
+if (isset($_GET['table'])) {
+  $selectedTable = $_GET['table'];
+} else {
+  $selectedTable = "alumnos"; // Default table
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,166 +28,110 @@ if (
    <h1>La NET del Profe</h1>
 	<div id="barratheadmin">
 		<ul>
-			<li><a href="#"><button class="alta">Alta</button></a></li>
+			<li><a href="?table=profes"><button class="alta">Profes</button></a></li>
+			<li><a href="?table=alumnos"><button class="alta">Alumnos</button></a></li>
 			<li><a href="#"><button class="botoeditar">Afegir</button></a></li>
 			<li><a href="#"><button class="botoeliminar">Borrar</button></a></li>
 			<li><a href="#"><button class="alta">Modificar</button></a></li>
 		</ul>
 	</div>
-    <div class="container">
-        <div class="admin-info">
-            <h2>Admin</h2>
-			<img src="admin.jpg" width="50%" height="50%">
-            <p>Nom: Administrador</p>
-            <p>Email: admin@gmail.es</p>
-            <p>Rol: Admin</p>
-        </div>
 
-        <div class="user-table">
-            <h2>Lista de Usuarios</h2>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-					<th>DNI/NIE</th>
-                    <th>Telefono</th>
-					<th>Rol</th>
-                    <th>Acciones</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Jeferson Rosas</td>
-                    <td>jeferson@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-						<button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Guillem Salas</td>
-                    <td>gillemsalas@fjeclot.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td> 
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Oriol Godoy</td>
-                    <td>oriol@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>4</td>
-                    <td>Marta Sanchez</td>
-                    <td>mrtasanches@fjeclot.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>5</td>
-                    <td>Jordi Sanchez</td>
-                    <td>lqsea@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>6</td>
-                    <td>José Luis Gil</td>
-                    <td>lqsea@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>7</td>
-                    <td>José Luis Gil</td>
-                    <td>lqsea@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>8</td>
-                    <td>José Luis Gil</td>
-                    <td>lqsea@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>9</td>
-                    <td>José Luis Gil</td>
-                    <td>lqsea@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-				<tr>
-                    <td>10</td>
-                    <td>José Luis Gil</td>
-                    <td>lqsea@fjebellvitge.edu</td>
-					<td>5454646K</td>
-					<td> 66666666 </td>
-                    <td>Estudiante</td>
-                    <td>
-                        <button class="alta">Alta</button>
-                        <button class="botoeditar">Editar</button>
-                        <button class="botoeliminar">Eliminar</button>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </div>
+	<?php if ($selectedTable === "profes"): ?>
+		<div class="container">
+			<div class="user-table">
+				<h2>Lista de Profes</h2>
+				<table>
+					<tr>
+						<th>ID</th>
+						<th>Nombre</th>
+						<th>Email</th>
+						<th>DNI/NIE</th>
+						<th>Telefono</th>
+						<th>Rol</th>
+						<th>Acciones</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Profesor 1</td>
+						<td>profesor1@example.com</td>
+						<td>1234567890</td>
+						<td>1234567890</td>
+						<td>Profesor</td>
+						<td>
+							<button class="alta">Alta</button>
+							<button class="botoeditar">Editar</button>
+							<button class="botoeliminar">Eliminar</button>
+						</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Profesor 2</td>
+						<td>profesor2@example.com</td>
+						<td>0987654321</td>
+						<td>0987654321</td>
+						<td>Profesor</td>
+						<td>
+							<button class="alta">Alta</button>
+							<button class="botoeditar">Editar</button>
+							<button class="botoeliminar">Eliminar</button>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	<?php else: ?>
+		<div class="container">
+			<div class="admin-info">
+				<h2>Admin</h2>
+				<img src="admin.jpg" width="50%" height="50%">
+				<p>Nom: Administrador</p>
+				<p>Email: admin@gmail.es</p>
+				<p>Rol: Admin</p>
+			</div>
+	
+			<div class="user-table">
+				<h2>Lista de Alumnos</h2>
+				<table>
+					<tr>
+						<th>ID</th>
+						<th>Nombre</th>
+						<th>Email</th>
+						<th>DNI/NIE</th>
+						<th>Telefono</th>
+						<th>Rol</th>
+						<th>Acciones</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Guillem Salas</td>
+						<td>jeferson@fjebellvitge.edu</td>
+						<td>5454646K</td>
+						<td>66666666</td>
+						<td>Estudiante</td>
+						<td>
+							<button class="alta">Alta</button>
+							<button class="botoeditar">Editar</button>
+							<button class="botoeliminar">Eliminar</button>
+						</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Jeferson Rosas</td>
+						<td>gillemsalas@fjeclot.edu</td>
+						<td>5454646K</td>
+						<td>66666666</td>
+						<td>Estudiante</td> 
+						<td>
+							<button class="alta">Alta</button>
+							<button class="botoeditar">Editar</button>
+							<button class="botoeliminar">Eliminar</button>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	<?php endif; ?>
+
 </body>
 </html>
 
